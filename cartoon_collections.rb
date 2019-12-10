eef52915a1173c87d61de6f61eb6254a9ddd9b3b
Brian Nicholls <nichol88@gmail.com>
2019-12-10 04:44:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)# code an argument here
   }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
 end
 
