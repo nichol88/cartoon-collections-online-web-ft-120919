5c9cb29ce1fd46fa6ac89df9707296d41031474a
Brian Nicholls <nichol88@gmail.com>
2019-12-10 04:56:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,15 @@ def summon_captain_planet(calls)# code an argument here
   }
 end
 
+#
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.each do |e|
+    if e.length > 4
+      return true
+    end
+  end
+  false
 end
 
 def find_the_cheese# code an argument here
