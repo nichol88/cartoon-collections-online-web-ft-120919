e9f250a35e2574238227d5ca95801c81166887f5
Brian Nicholls <nichol88@gmail.com>
2019-12-10 04:57:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def long_planeteer_calls(calls)# code an argument here
   false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
